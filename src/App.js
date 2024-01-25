86273417bee107c9acea8e2687f7263ef894a14f
Nick Buckingham <nbuckingham33@gmail.com>
2024-01-24 22:10:37 -0800

    Project started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      {/* Other components and content go here */}
+    </div>
+  );
+};
+
+export default App;
